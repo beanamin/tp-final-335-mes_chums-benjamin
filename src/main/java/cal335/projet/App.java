d8f08e9e88818490c19f669eb516b93b63fd7404
bleon <bleonbliss@gmail.com>
2024-11-26 17:09:18 -0500

    Début de mes_chums

---
+++ src/main/java/cal335/projet/App.java
@@ -0,0 +1,13 @@
+package cal335.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
